2255d9dd693360ff29879fd4f7ec395ab8ae1816
yangxj-154 <yangxj154@gmail.com>
2026-06-16 10:56:16 +0800

    更新SW缓存版本号到v26，强制刷新客户端缓存

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='caihua-cal-v1';
+var CACHE='caihua-cal-v26';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));
